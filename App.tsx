d7251ed94c8ee950ff40b2dfc1a028308b23495e
grin-quokka <grinquokka@gmail.com>
2020-01-02 13:36:58 +0900

    :tada: init setting react-native & typescript

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+declare var global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <View>
+      <Text>init</Text>
+    </View>
+  );
+};
+
+export default App;
