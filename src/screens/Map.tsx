ecdab3834fc43ea67d0675511533e5dd42524925
grin-quokka <grinquokka@gmail.com>
2020-01-11 17:30:28 +0900

    :construction: naver map 띄우기

--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -1,13 +1,21 @@
 import React, {ReactElement} from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import homeStyle from '../styles/homeStyle';
+import NaverMapView, {Marker, Coord} from 'react-native-nmap';
 
 interface Props {}
 
 export default function Map({}: Props): ReactElement {
+  const coord: Coord = {latitude: 37.5670135, longitude: 126.978374};
+
   return (
     <View style={homeStyle.mapStyle}>
-      <Text>여기는 지도 자리</Text>
+      <NaverMapView
+        style={{width: '100%', height: '100%'}}
+        showsMyLocationButton={true}
+        center={coord}>
+        <Marker coordinate={coord} />
+      </NaverMapView>
     </View>
   );
 }
