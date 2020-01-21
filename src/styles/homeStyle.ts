f54a0a3fc86bdb00eac94bdcd148c48efdf3c573
최방실 <qkdtlf89089@gmail.com>
2020-01-21 19:25:14 +0900

    :art: 스타일 시트 수정(홈/디테일뷰/사인업)
    1. 홈: 탭뷰모드에 적용에 따른 화면 전환시 딜레이 이슈 핸들링 위해 로딩 화면(스타일) 추가
    2. 디테일뷰: 디테일뷰 랜더링 테스트용 페이크데이터 삭제
    3. 사인업: src 폴더 style 파일 -> 스타일 폴더로 이동 및 생성

--- src/styles/homeStyle.ts
+++ src/styles/homeStyle.ts
@@ -1,6 +1,7 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
+  loading: {flex: 1, justifyContent: 'center', alignItems: 'center'},
   home: {flex: 1},
   mapStyle: {flex: 1},
   listStyle: {flex: 1},
