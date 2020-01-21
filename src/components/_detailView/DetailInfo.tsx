43c011523ed70945259c647dece9b6b21485f468
최방실 <qkdtlf89089@gmail.com>
2020-01-21 20:17:23 +0900

    :construction: 회원가입 validator 실행 타이밍 수정

---
+++ src/components/_detailView/DetailInfo.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View, Text, ScrollView} from 'react-native';
+import Style from '../../styles/detailViewStyle';
+import {PetCard, WalkCard, PetAvatarList} from '../detailView';
+
+interface Props {
+  props: Object;
+}
+
+const DetailInfo = ({props}: Props) => {
+  const {selectPet, selectedPet, list}: any = props;
+  return (
+    <View style={{flex: 1}}>
+      <ScrollView>
+        <View style={Style.section}>
+          <WalkCard />
+        </View>
+        <View style={Style.petListSec}>
+          <PetAvatarList selectPet={selectPet} list={list} />
+        </View>
+        <View style={Style.section}>
+          <PetCard selectedPet={selectedPet} />
+        </View>
+        <View style={Style.mapSec}>
+          <Text style={Style.sectionTitle}>map</Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default DetailInfo;
