5666bbce66b0cdfffff2c8eb02ad1ce0ef533a74
최방실 <qkdtlf89089@gmail.com>
2020-01-21 19:49:23 +0900

    :sparkle: 스크린 포커싱에 따른 탭뷰모드 동적 변경

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, ActivityIndicator} from 'react-native';
 import Map from './Map';
 import List from './List';
 import homeStyle from '../styles/homeStyle';
 import {NavigationAction} from '@react-navigation/native';
+import {viewTab} from '../redux/navOption';
+import {connect} from 'react-redux';
+import {useIsFocused} from '@react-navigation/native';
 
 interface Props {
   navigation: NavigationAction;
+  viewTabbar: Function;
 }
-const Home = ({navigation}: Props) => {
-  return (
+const Home = ({navigation, viewTabbar}: Props) => {
+  const isFocused = useIsFocused();
+  let loaded = false;
+  const render = () => {
+    viewTabbar();
+    loaded = true;
+  };
+
+  isFocused ? render() : null;
+  return loaded ? (
     <View style={homeStyle.home}>
       <Map />
       <List navigation={navigation} />
     </View>
+  ) : (
+    <View style={homeStyle.loading}>
+      <ActivityIndicator />
+    </View>
   );
 };
 
-export default Home;
+const mapDispatchToProps = dispatch => ({
+  viewTabbar: () => dispatch(viewTab()),
+});
+
+export default connect(null, mapDispatchToProps)(Home);
