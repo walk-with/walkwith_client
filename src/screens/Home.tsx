0473f4b314a5dbb602d2e0831adfb7d2016e2773
grin-quokka <grinquokka@gmail.com>
2020-01-23 20:10:27 +0900

    :sparkles: 지도 움직이면 리랜더링, 마커 추가, 리스트 포커스

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,16 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View} from 'react-native';
 import Map from './Map';
 import List from './List';
 import homeStyle from '../styles/homeStyle';
 
-const Home = () => {
-  return (
-    <View style={homeStyle.home}>
-      <Map />
-      <List />
-    </View>
-  );
-};
+interface Props {}
+interface State {
+  curMarker: number;
+}
+export default class Home extends Component<Props, State> {
+  list = [
+    {
+      name: '오구',
+      avatar_url:
+        'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      subtitle: 'Vice President',
+      coord: {latitude: 37.543, longitude: 126.8664283},
+    },
+    {
+      name: '뭉치',
+      avatar_url:
+        'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Vice Chairman',
+      coord: {latitude: 37.564, longitude: 126.87},
+    },
+    {
+      name: '예삐',
+      avatar_url:
+        'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      subtitle: 'Vice President',
+      coord: {latitude: 37.55, longitude: 126.85},
+    },
+    {
+      name: '초코',
+      avatar_url:
+        'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      subtitle: 'Vice President',
+      coord: {latitude: 37.55, longitude: 126.85},
+    },
+  ];
 
-export default Home;
+  state = {curMarker: -1};
+
+  changeCurMarker = (id: number) => {
+    this.setState({curMarker: id});
+  };
+
+  render() {
+    const {curMarker} = this.state;
+    return (
+      <View style={homeStyle.home}>
+        <Map list={this.list} changeCurMarker={this.changeCurMarker} />
+        <List list={this.list} curMarker={curMarker} />
+      </View>
+    );
+  }
+}
--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -2,20 +2,38 @@ import React, {Component} from 'react';
 import {View} from 'react-native';
 import homeStyle from '../styles/homeStyle';
 import Geolocation from 'react-native-geolocation-service';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
-interface Props {}
+interface Props {
+  list: Array<{
+    name: string;
+    avatar_url: string;
+    subtitle: string;
+    coord: {latitude: number; longitude: number};
+  }>;
+  changeCurMarker: (id: number) => void;
+}
 interface State {
-  coord: Coord;
+  coord: {latitude: number; longitude: number};
+  latitudeDelta: number;
+  longitudeDelta: number;
 }
 
 export default class Map extends Component<Props, State> {
-  state = {coord: {latitude: 37.5670135, longitude: 126.978374}};
+  state = {
+    coord: {latitude: 37.5670135, longitude: 126.978374},
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
 
   componentDidMount() {
     Geolocation.getCurrentPosition(
       ({coords}) => {
-        this.setState({
-          coord: {latitude: coords.latitude, longitude: coords.longitude},
+        this.setState(prev => {
+          return {
+            ...prev,
+            coord: {latitude: coords.latitude, longitude: coords.longitude},
+          };
         });
       },
       error => {
@@ -26,7 +44,54 @@ export default class Map extends Component<Props, State> {
   }
 
   render() {
-    const {coord} = this.state;
-    return <View style={homeStyle.mapStyle} />;
+    const {coord, latitudeDelta, longitudeDelta} = this.state;
+    return (
+      <View style={homeStyle.mapStyle}>
+        <MapView
+          style={homeStyle.mapStyle}
+          provider={PROVIDER_GOOGLE}
+          region={{
+            ...coord,
+            latitudeDelta,
+            longitudeDelta,
+          }}
+          showsUserLocation={true}
+          onRegionChangeComplete={region => {
+            this.setState({
+              coord: {
+                latitude: region.latitude,
+                longitude: region.longitude,
+              },
+              latitudeDelta: region.latitudeDelta,
+              longitudeDelta: region.longitudeDelta,
+            });
+          }}>
+          <Marker
+            key={4}
+            coordinate={{...coord}}
+            title={'움직여'}
+            description="this is a marker example"
+            onPress={e => {
+              // 문자 -> 숫자로 변환해서 state 변경
+              this.props.changeCurMarker(Number(e.nativeEvent.id));
+            }}
+            identifier={`${3}`}
+          />
+          {this.props.list.map((marker, i) => (
+            <Marker
+              key={i}
+              coordinate={{...marker.coord}}
+              title={marker.name}
+              description="this is a marker example"
+              onPress={e => {
+                // 문자 -> 숫자로 변환해서 state 변경
+                this.props.changeCurMarker(Number(e.nativeEvent.id));
+              }}
+              identifier={`${i}`}
+            />
+          ))}
+        </MapView>
+      </View>
+    );
   }
 }
